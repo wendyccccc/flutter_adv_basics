611ccc2acda60618258b4236bb668b264ae1e276
Wendy Private <lienyun.chang@gmail.com>
2024-09-11 10:11:07 +0800

    refactor: 重構頁面

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                colors: [Color.fromARGB(255, 69, 29, 139), Colors.deepPurple],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
