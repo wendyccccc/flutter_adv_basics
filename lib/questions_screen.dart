9c7d5d5aa78ec0d9f73175e86058875f5a31bd14
Wendy Private <lienyun.chang@gmail.com>
2024-09-11 16:52:21 +0800

    feat: 題目渲染至螢幕上

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,8 @@
+import 'package:adv_basics/data/questions.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:adv_basics/answer_button.dart';
+import 'package:adv_basics/models/quiz_question.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,6 +16,25 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('QuestionsScreen');
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity, // 越寬越好
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 30),
+          ...currentQuestion.answers.map((item) {
+            return AnswerButton(answerText: item, onTap: () {});
+          }),
+        ],
+      ),
+    );
   }
 }
