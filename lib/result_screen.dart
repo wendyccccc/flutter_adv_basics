6e8ef32c77997418367755840516b4c55d803bad
Wendy Private <lienyun.chang@gmail.com>
2024-09-12 16:14:23 +0800

    85. 製作結果頁面

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/questions_summary.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key, required this.chosenAnswers});
 
   final List<String> chosenAnswers;
+
+  // Map: <key:value>
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i]
+      });
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +34,7 @@ class ResultScreen extends StatelessWidget {
             children: [
               const Text('你答對了X題！'),
               const SizedBox(height: 30),
-              const Text('問題與答案列表'),
+              QuestionsSummary(getSummaryData()),
               const SizedBox(height: 30),
               TextButton(
                 onPressed: () {},
