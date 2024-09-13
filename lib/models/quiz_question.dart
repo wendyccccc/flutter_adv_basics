9db3570ad6dca257aa428a74d6ed4bce21a25154
Wendy Private <lienyun.chang@gmail.com>
2024-09-13 11:17:57 +0800

    89. getter & arrow function

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -4,8 +4,8 @@ class QuizQuestion {
   final String text;
   final List<String> answers;
 
-  List<String> getShuffledAnswers() {
-    final shuffledList = List.of(answers); // of: 複製列表
+  List<String> get shuffledAnswers {
+    final shuffledList = List.of(answers);
     shuffledList.shuffle();
     return shuffledList;
   }
--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -14,7 +14,7 @@ class ResultScreen extends StatelessWidget {
   final void Function() backHome;
 
   // Map: <key:value>
-  List<Map<String, Object>> getSummaryData() {
+  List<Map<String, Object>> get summaryData {
     final List<Map<String, Object>> summary = [];
 
     for (var i = 0; i < chosenAnswers.length; i++) {
@@ -31,11 +31,11 @@ class ResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final summaryData = getSummaryData();
+    // final summaryData = getSummaryData();
     final numTotalQuestions = questions.length;
-    final numCorrectQuestions = summaryData.where((data) {
-      return data['user_answer'] == data['correct_answer'];
-    }).length;
+    final numCorrectQuestions = summaryData
+        .where((data) => data['user_answer'] == data['correct_answer'])
+        .length;
 
     return SizedBox(
         width: double.infinity,
@@ -51,7 +51,7 @@ class ResultScreen extends StatelessWidget {
                     fontSize: 24,
                   )),
               const SizedBox(height: 30),
-              QuestionsSummary(getSummaryData()),
+              QuestionsSummary(summaryData),
               const SizedBox(height: 30),
               TextButton.icon(
                 style: TextButton.styleFrom(
